095608c620d6a97ae2a47a7b1ad17f9a10b9e7af
Federico D'Angelo <federicodangelo@gmail.com>
2013-11-16 22:48:52 -0300

    Initial import (base GDX project)

---
+++ circleworld-assets/src/AssetsUpdater.java
@@ -0,0 +1,11 @@
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+
+
+public class AssetsUpdater {
+
+	public static void main(String[] args) {
+		
+		TexturePacker2.process("atlas", "assets/atlas", "atlas");
+		
+	}
+}
